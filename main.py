c557e0d60837a3d3f7e63754c737e22ba0c0b6c9
TheElderElf <sambucuself@gmail.com>
2025-04-07 08:02:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ def speak(text):
     try:
         import re
         plain_text = re.sub(r'[\*_`>#\-]', '', text)  # Strip simple Markdown
-        plain_text = re.sub(r'[\U0001F600-\U0001F64F\U0001F300-\U0001F5FF\U0001F680-\U0001F6FF\U0001F700-\U0001F77F\U0001F780-\U0001F7FF\U0001F800-\U0001F8FF\U0001F900-\U0001F9FF\U0001FA00-\U0001FA6F\U0001FA70-\U0001FAFF\U00002600-\U000026FF\U00002700-\U000027BF]+', '', plain_text
         engine.say(plain_text)
         engine.runAndWait()
     except Exception as e:
